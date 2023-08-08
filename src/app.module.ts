d7544b005bd83b88c436b3f573496ec6d87fde9f
JorgeSaicoski <jorgesaicoski@gmail.com>
2023-08-07 22:47:40 -0300

    structure folder change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ShowroomModule } from './showroom/showroom.module';
-import { ValuesModule } from './values/values.module';
-import { ProductsModule } from './products/products.module';
-import { CoinsModule } from './coins/coins.module';
-import { ContactModule } from './contact/contact.module';
-import { DescriptionsModule } from './descriptions/descriptions.module';
-import { TextsModule } from './texts/texts.module';
-import { LanguagesModule } from './languages/languages.module';
+import { ShowroomModule } from './modules/showroom/showroom.module';
+import { ValuesModule } from './modules/values/values.module';
+import { ProductsModule } from './modules/products/products.module';
+import { CoinsModule } from './modules/coins/coins.module';
+import { ContactModule } from './modules/contact/contact.module';
+import { DescriptionsModule } from './modules/descriptions/descriptions.module';
+import { TextsModule } from './modules/texts/texts.module';
+import { LanguagesModule } from './modules/languages/languages.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ProjectsModule } from './projects/projects.module';
+import { ProjectsModule } from './modules/projects/projects.module';
+import { MongodbService } from './mongodb/mongodb.service';
 
 @Module({
   imports: [
@@ -26,6 +27,6 @@ import { ProjectsModule } from './projects/projects.module';
     ValuesModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MongodbService],
 })
 export class AppModule {}
